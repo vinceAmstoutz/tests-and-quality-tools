dc8b65b86c30477760d5d9b7ad15c7c4c743d467
vinceAmstoutz <vincent.amstoutz@outlook.fr>
2022-10-07 17:38:02 +0200

    WIP - Add simple PHPUnit test case

---
+++ tests/PHPUnit/Controller/EmailTestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\PHPUnit\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class EmailTestController extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
