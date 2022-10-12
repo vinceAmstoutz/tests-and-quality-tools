78954b2cad19da5936b518438bc75995c07ac30b
vinceAmstoutz <vincent.amstoutz@outlook.fr>
2022-10-12 14:38:12 +0200

    First basic unit test

---
+++ tests/AppTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+
+    public function testTestAreWorking(): void
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+}
