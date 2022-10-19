ca14328f506feba6b0cfc3ace6946a57471c9ba1
vinceAmstoutz <vincent.amstoutz@outlook.fr>
2022-10-19 11:04:58 +0200

    Add auto reset database for tests

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
