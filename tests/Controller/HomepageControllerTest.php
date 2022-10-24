c04246e6f1579d47eb61aa02a1c0be42feb777e2
vinceAmstoutz <vincent.amstoutz@outlook.fr>
2022-10-24 10:05:26 +0200

    up : add basic web test case suite

---
+++ tests/Controller/HomepageControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomepageControllerTest extends WebTestCase
+{
+    public function testHomepagePage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testH1HomepagePage(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertSelectorTextSame('h1', 'Hello world');
+    }
+}
