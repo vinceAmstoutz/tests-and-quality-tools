afdd15e733fd61623d3fc024b3184077acf70bb9
vinceAmstoutz <vincent.amstoutz@outlook.fr>
2022-11-10 11:03:18 +0100

    feat: add feature context for Behat features

--- tests/Behat/FeatureContext.php
+++ tests/Behat/FeatureContext.php
@@ -10,18 +10,13 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat;
 
+use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\MinkContext;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-/**
- * This context class contains the definitions of the steps used by the demo
- * feature file. Learn how to get started with Behat and BDD on Behat's website.
- *
- * @see http://behat.org/en/latest/quick_start.html
- */
-final class DemoContext extends MinkContext
+final class FeatureContext extends MinkContext implements Context
 {
     /** @var KernelInterface */
     private $kernel;
