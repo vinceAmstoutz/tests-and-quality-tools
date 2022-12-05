ebed03226e546ea8d848b048b515fd9569e48e20
vinceAmstoutz <vincent.amstoutz@outlook.fr>
2022-12-05 16:57:39 +0100

    fix: using IsGranted from FrameworkExtraBundle instead of SF component

--- src/Controller/FakeSecurityController.php
+++ src/Controller/FakeSecurityController.php
@@ -10,10 +10,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class FakeSecurityController extends AbstractController
